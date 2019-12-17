e9b91423602bf9cfb28401bc20e5618cab2f81b7
MarvinSequera <marvinsequera@yahoo.com>
2019-12-17 14:42:22 +0100

    por 301

---
+++ server/public/precache-manifest.913b54ceef03a55dda73523179c19607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50cf5591252eb22d6b3584ca042e7ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0ce8873724d5ecf20e",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "93fb6aabf8ba1a79f6f3",
+    "url": "/static/css/main.f6dfecd6.chunk.css"
+  },
+  {
+    "revision": "3c0ce8873724d5ecf20e",
+    "url": "/static/js/2.0ec32ea4.chunk.js"
+  },
+  {
+    "revision": "57c78daa3ca1811e5f339e6c4dc38e66",
+    "url": "/static/js/2.0ec32ea4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93fb6aabf8ba1a79f6f3",
+    "url": "/static/js/main.fcdc2d46.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
